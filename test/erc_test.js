978aa94834ab88bab2ad5a40c92423d47110a5ac
GarryFCR <garryranee@gmail.com>
2021-04-23 18:14:12 +0530

    Tests for optional methods

--- test/erc_test.js
+++ test/erc_test.js
@@ -2,7 +2,22 @@
 var Alphacointract = artifacts.require("Alphacointract.sol");
 
 contract( 'Alphacointract', function(accounts){
+    var tokeninstance;
 
+    it('Sets the optional variables',function(){
+        return Alphacointract.deployed().then(function(instance){
+            tokeninstance=instance;
+            return tokeninstance.name();
+            }).then(function(name){
+                assert.equal(name,'Alphacoin','sets the coin name');
+                return tokeninstance.symbol();
+                }).then(function(symbol){
+                assert.equal(symbol,'ALPH','sets the symbol');
+                return tokeninstance.decimals();
+                }).then(function(decimal){
+                assert.equal(decimal,18,'sets the decimal');
+                });
+        })
     it('Set up the total supply upon deployment', function(){
         return Alphacointract.deployed().then(function(instance){
             tokeninstance = instance;
