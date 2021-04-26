6138df936aba272dde35bd63a213a506c7c71a7f
GarryFCR <garryranee@gmail.com>
2021-04-26 20:39:47 +0530

    Started with the Ico contract

--- contracts/Alphacointract.sol
+++ contracts/Alphacointract.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 // ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
+// 
 //
 // ----------------------------------------------------------------------------
 
@@ -18,6 +18,11 @@ contract SafeMath {
         require(b <= a); 
         c = a - b;
          } 
+    function safeMul(uint a, uint b) public pure returns (uint c) { 
+        c = a * b; 
+        require(a == 0 || c / a == b); 
+        
+    } 
     
 }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const Alphacointract = artifacts.require("Alphacointract.sol");
+const Ico = artifacts.require("Ico.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Alphacointract);
-};
+  deployer.deploy(Alphacointract).then(function(){
+  	  return deployer.deploy(Ico, Alphacointract.address);
+  });
+
+}
